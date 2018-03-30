56cf1b3aa920ecfe657e49eb3895546fb0a6be92
TurkeyMcMac <jwmhjwmh@gmail.com>
2018-03-29 20:51:02 -0400

    Split brain mutation and such into a separate unit

--- src/animal.c
+++ src/animal.c
@@ -1,50 +1,10 @@
 #include "animal.h"
 
+#include "brain.h"
 #include "grid.h"
 #include <limits.h>
 #include <stdlib.h>
 #include <string.h>
-
-#define OP_INFO(name, energy) [OP_##name] = {#name, energy}
-
-#define GNRG_COST 1
-
-const struct opcode_info op_info[N_OPCODES] = {
-	/*	Name	Energy */
-	OP_INFO(MOVE,	     1),
-	OP_INFO(XCHG,	     2),
-	OP_INFO(GFLG,	     1),
-	OP_INFO(SFLG,	     1),
-	OP_INFO(GIPT,	     1),
-	OP_INFO(AND,	     4),
-	OP_INFO(OR,	     4),
-	OP_INFO(XOR,	     4),
-	OP_INFO(NOT,	     3),
-	OP_INFO(SHFR,	     4),
-	OP_INFO(SHFL,	     4),
-	OP_INFO(ADD,	     6),
-	OP_INFO(SUB,	     6),
-	OP_INFO(INCR,	     4),
-	OP_INFO(DECR,	     4),
-	OP_INFO(JUMP,	     4),
-	OP_INFO(CMPR,	     6),
-	OP_INFO(JMPA,	     6),
-	OP_INFO(JPNA,	     6),
-	OP_INFO(JMPO,	     6),
-	OP_INFO(JPNO,	     6),
-	OP_INFO(PICK,	     8),
-	OP_INFO(DROP,	     7),
-	OP_INFO(LCHM,	     7),
-	OP_INFO(LNML,	     7),
-	OP_INFO(BABY,	     5),
-	OP_INFO(STEP,	     7),
-	OP_INFO(ATTK,	     7),
-	OP_INFO(CONV,	     6),
-	OP_INFO(EAT,	     2),
-	OP_INFO(GCHM,	     2),
-	OP_INFO(GHLT,	     1),
-	OP_INFO(GNRG,GNRG_COST),
-};
 
 #define bits_on(bitset, bits) ((bitset) |= (bits))
 #define bits_off(bitset, bits) ((bitset) &= ~(bits))
@@ -554,62 +514,6 @@ void animal_step(struct animal *self, struct grid *g, size_t x, size_t y)
 	sub_saturate(&self->energy, op_info[instr.opcode].energy);
 error:
 	++self->instr_ptr;
-}
-
-struct brain *brain_new(uint16_t signature, uint16_t ram_size, uint16_t code_size)
-{
-	struct brain *self = malloc(sizeof(struct brain) + code_size * sizeof(struct instruction));
-	self->next = NULL;
-	self->refcount = 0;
-	self->signature = signature;
-	self->ram_size = ram_size;
-	self->code_size = code_size;
-	memset(self->code, -1, code_size * sizeof(struct instruction));
-	return self;
-}
-
-static struct brain *copy_brain(struct brain *b)
-{
-	struct brain *c = malloc(sizeof(*b) + b->code_size * sizeof(*b->code));
-	memcpy(c, b, sizeof(*b) + b->code_size * sizeof(*b->code));
-	c->refcount = 0;
-	c->next = NULL;
-	return c;
-}
-
-#define MKIND_CASE_INSTR_FIELD(kind, field) \
-	case MKIND_##kind: { \
-		b = copy_brain(self); \
-		b->code[grid_rand(g) % self->code_size].field = grid_rand(g); \
-	} break
-
-#define MKIND_CASE_INSTR_FMT(kind, field) \
-	case MKIND_##kind: { \
-		b = copy_brain(self); \
-		uint8_t r = grid_rand(g); \
-		size_t idx = grid_rand(g) % self->code_size; \
-		if (b->code[idx].field == r % 4) \
-			++r; \
-		b->code[idx].field = r; \
-	} break
-
-struct brain *brain_mutate(struct brain *self, struct grid *g)
-{
-	struct brain *b;
-	switch(grid_rand(g) % 6) {
-	case MKIND_SIGNATURE: {
-		b = copy_brain(self);
-		b->signature = grid_rand(g);
-	} break;
-	MKIND_CASE_INSTR_FIELD(OPCODE, opcode);
-	MKIND_CASE_INSTR_FMT(INSTR_LFMT, l_fmt);
-	MKIND_CASE_INSTR_FMT(INSTR_RFMT, r_fmt);
-	MKIND_CASE_INSTR_FIELD(INSTR_LEFT, left);
-	MKIND_CASE_INSTR_FIELD(INSTR_RIGHT, right);
-	}
-	b->next = g->species;
-	g->species = b;
-	return b;
 }
 
 struct animal *animal_new(struct brain *brain, uint16_t energy)
--- src/animal.h
+++ src/animal.h
@@ -2,54 +2,11 @@
 
 #define _ANIMAL_H
 
+#include "brain.h"
 #include "chemicals.h"
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-
-#define SLLIST_FOR_EACH(list, item) for ((item) = (list); (item) != NULL; (item) = (item)->next)
-
-struct instruction {
-	uint8_t opcode;
-	uint8_t l_fmt : 2,
-		r_fmt : 2;
-	uint16_t left, right;
-};
-
-struct brain {
-	struct brain *next;
-	size_t refcount;
-	uint32_t save_num;
-	uint16_t signature;
-	uint16_t ram_size, code_size;
-	struct instruction code[];
-};
-
-struct brain *brain_new(uint16_t signature, uint16_t ram_size, uint16_t code_size);
-
-enum {
-	MKIND_SIGNATURE,
-	MKIND_OPCODE,
-	MKIND_INSTR_LFMT,
-	MKIND_INSTR_RFMT,
-	MKIND_INSTR_LEFT,
-	MKIND_INSTR_RIGHT,
-	MKIND_ADD,
-	MKIND_REMOVE_ONE,
-	MKIND_REMOVE_MANY,
-	MKIND_DUP_ONE,
-	MKIND_DUP_MANY,
-	MKIND_SWAP_ONE,
-	MKIND_SWAP_MANY,
-	MKIND_REPLACE,
-	MKIND_RAM_SIZE,
-
-	N_MKIND
-};
-
-struct grid;
-
-struct brain *brain_mutate(struct brain *self, struct grid *g);
 
 struct animal {
 	struct brain *brain;
@@ -71,68 +28,5 @@ void animal_step(struct animal *self, struct grid *grid, size_t x, size_t y);
 bool animal_is_dead(const struct animal *self);
 
 void animal_free(struct animal *self);
-
-enum opcode {
-/* General */
-	OP_MOVE,	/* dest src */
-	OP_XCHG,	/* dest1 dest2 */
-	OP_GFLG,	/* dest */
-	OP_SFLG,	/* src */
-	OP_GIPT,	/* dest */
-/* Bitwise */
-	OP_AND,		/* dest src */
-	OP_OR,		/* dest src */
-	OP_XOR,		/* dest src */
-	OP_NOT,		/* flip */
-	OP_SHFR,	/* dest amount */
-	OP_SHFL,	/* dest amount */
-/* Arithmetic */
-	OP_ADD,		/* dest src */
-	OP_SUB,		/* dest src */
-	OP_INCR,	/* increment */
-	OP_DECR,	/* decrement */
-/* Control flow */
-	OP_JUMP,	/* dest */
-	OP_CMPR,	/* cmp1 cmp2 */
-	OP_JMPA,	/* dest test */
-	OP_JPNA,	/* dest test */
-	OP_JMPO,	/* dest test */
-	OP_JPNO,	/* dest test */
-/* Special */
-	OP_PICK,	/* direction num:8,id:8 */
-	OP_DROP,	/* direction num:8,id:8 */
-	OP_LCHM,	/* dest id:6,x:5,y:5 */
-	OP_LNML,	/* dest _:6,x:5,y:5 */
-	OP_BABY,	/* direction energy */
-	OP_STEP,	/* direction */
-	OP_ATTK,	/* direction power */
-	OP_CONV,	/* id1 id2 */
-	OP_EAT,		/* id num */
-	OP_GCHM,	/* dest id */
-	OP_GHLT,	/* dest */
-	OP_GNRG,	/* dest */
-
-	N_OPCODES
-};
-
-enum flags {
-	FEQUAL = 1 << 0,
-	FUGREATER = 1 << 1,
-	FULESSER = 1 << 2,
-	FSGREATER = 1 << 3,
-	FSLESSER = 1 << 4,
-	FINVAL_ARG = 1 << 5,
-	FROOB = 1 << 6,
-	FCOOB = 1 << 7,
-	FINVAL_OPCODE = 1 << 8,
-	FEMPTY = 1 << 9,
-	FFULL = 1 << 10,
-	FBLOCKED = 1 << 11,
-};
-
-extern const struct opcode_info {
-	char name[5];
-	uint16_t energy;
-} op_info[N_OPCODES];
 
 #endif /* Header guard */
