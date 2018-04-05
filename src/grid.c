5e968a0d2018d6bd8d2ee6234659b860e75f3e31
TurkeyMcMac <jwmhjwmh@gmail.com>
2018-04-04 22:22:09 -0400

    Shorten the lines of grid.*, too!

--- src/grid.c
+++ src/grid.c
@@ -51,7 +51,8 @@ struct tile *grid_get(struct grid *self, size_t x, size_t y)
 		return NULL;
 }
 
-const struct tile *grid_get_const_unck(const struct grid *self, size_t x, size_t y)
+const struct tile *grid_get_const_unck(const struct grid *self,
+	size_t x, size_t y)
 {
 	return &self->tiles[y * self->width + x];
 }
@@ -103,7 +104,8 @@ void grid_draw(const struct grid *self, FILE *dest)
 			const struct tile *t = grid_get_const_unck(self, x, y);
 			print_color(t, dest);
 			if (t->animal)
-				fprintf(dest, "\x1B[37m[]\x1B[38;5;"EMPTY_GRAY"m");
+				fprintf(dest,
+					"\x1B[37m[]\x1B[38;5;"EMPTY_GRAY"m");
 			else
 				fprintf(dest, "[]");
 		}
@@ -140,7 +142,8 @@ static uint16_t init_evaporation_mask(uint16_t tick)
 	return evaporating;
 }
 
-static void flow_fluids(uint16_t flowing, struct grid *g, struct tile *t, size_t x, size_t y)
+static void flow_fluids(uint16_t flowing, struct grid *g, struct tile *t,
+	size_t x, size_t y)
 {
 	uint16_t idx;
 	for (; flowing != 0; flowing &= flowing - 1) {
@@ -234,8 +237,10 @@ void grid_update(struct grid *self)
 	update_tiles(self);
 	free_extinct(self);
 	if (self->tick % self->drop_interval == 0) {
-		grid_get_unck(self, grid_rand(self) % self->width, grid_rand(self) % self->height)
-			->chemicals[grid_rand(self) % 3 + 1] = self->drop_amount;
+		grid_get_unck(self,
+			grid_rand(self) % self->width,
+			grid_rand(self) % self->height
+		)->chemicals[grid_rand(self) % 3 + 1] = self->drop_amount;
 	}
 	++self->tick;
 }
--- src/grid.h
+++ src/grid.h
@@ -52,7 +52,8 @@ struct tile *grid_get_unck(struct grid *self, size_t x, size_t y);
 
 struct tile *grid_get(struct grid *self, size_t x, size_t y);
 
-const struct tile *grid_get_const_unck(const struct grid *self, size_t x, size_t y);
+const struct tile *grid_get_const_unck(const struct grid *self,
+	size_t x, size_t y);
 
 const struct tile *grid_get_const(const struct grid *self, size_t x, size_t y);
 
